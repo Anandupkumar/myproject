5fe0018a74f1ab9da56b922eb23e533ae1a15eac
Anandu p kumar <anandupkumar99@gmail.com>
2022-03-25 12:09:34 +0530

    completed login by retreaving username and password from database and checking

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+    $conn=new mysqli("localhost","root","","mydatabase");
+    if(!$conn){
+        die("Connection failed: " . $conn->connect_error);
+    }else{
+            $uname = $password = "";
+            $uname=$_POST["uname"];
+            $password=$_POST["psw"];
+            $sql="SELECT * FROM users WHERE name= '$uname' AND PASSWORD= '$password'";
+            $result=$conn->query($sql);
+            if($result->num_rows >0){
+                $row=$result->fetch_assoc();
+                if($row['name']== $uname && $row['password']== $password){
+                    include('afterlogin.html');
+                }
+            }else{
+                include('index.html');
+                echo '<script>alert("invalid username or password !");</script>';
+            }
+            
+    }
+?>
